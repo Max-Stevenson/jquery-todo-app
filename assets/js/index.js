9a7346390bb9aa83b8620d8e64a3710f86ff2a5e
Max Stevenson <maxstevenson@msn.com>
2019-12-04 14:47:30 +0000

    Function created for removing todo items.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
-$("li").click(function(){
+$("li").click(function() {
   $(this).toggleClass("completed");
+});
+
+$("span").click(function(event) {
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
 });
