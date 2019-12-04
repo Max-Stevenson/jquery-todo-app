51bef68535014c7d095a2364f81fb3630c7e9edb
Max Stevenson <maxstevenson@msn.com>
2019-12-04 15:25:57 +0000

    Restructure to html.
    - Space included in span element for display.
    - Added method to add new todo items.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,18 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   event.stopPropagation();
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    let todoText = $(this).val();
+    $(this).val("");
+    $("ul").append(`<li><span>X </span>${todoText}</li>`)
+  };
 });
